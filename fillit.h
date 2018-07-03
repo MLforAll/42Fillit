b075f6753d54aa98cf634fb26cd5833b0df6a0ca
Kelian DUMARAIS <kdumarai@student.42.fr>
2018-07-03 06:35:26 +0200

    Readded source files

---
+++ fillit.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdumarai <kdumarai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/14 15:29:51 by kdumarai          #+#    #+#             */
+/*   Updated: 2018/04/25 13:49:57 by kdumarai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+# include "libft.h"
+
+int		is_file_valid(char **allpieces);
+
+int		malloc_board_size(char ***board, int bounds);
+int		solve_fillit(char **piece, char ***board, int bounds, char letter);
+
+int		get_piece_start(char **allpieces);
+int		strchrs(const char *str, const char *chars);
+int		get_min_size(int n_pieces);
+char	*strnewc(int size, char c);
+int		cnt_char(char *str, char c);
+
+#endif
